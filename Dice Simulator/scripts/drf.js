4899c05981c6492dda2e54f1bedb80f28289c83f
IanEarley <iearley88@gmail.com>
2023-05-10 00:51:50 -0500

    refactor of dice project

---
+++ Dice Simulator/scripts/drf.js
@@ -0,0 +1,7 @@
+let num = get_number("Enter the number of rolls:");
+let results = roll_dice(num);
+
+let table = document.getElementById("results");
+log_results(num, results, table);
+
+table.classList.remove("hide");
